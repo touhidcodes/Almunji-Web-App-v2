cd9291de6da48743fd7236ff190d3719606d1b6a
touhidcodes <touhidcodes@gmail.com>
2025-11-28 21:20:23 +0600

    mod: register form

--- src/components/pages/Auth/RegisterForm.tsx
+++ src/components/pages/Auth/RegisterForm.tsx
@@ -1,13 +1,11 @@
-// components/Auth/RegisterForm.tsx
 "use client";
 
 import FormContainer from "@/components/forms/FormContainer";
 import FormInput from "@/components/forms/FormInput";
-import FormSelect from "@/components/forms/FormSelect";
 import { Button } from "@/components/ui/button";
 import { registerValidationSchema } from "@/schema/authSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Loader2 } from "lucide-react";
+import { ArrowRight, Loader2 } from "lucide-react";
 import Link from "next/link";
 import { FieldValues } from "react-hook-form";
 import { z } from "zod";
@@ -17,6 +15,7 @@ interface RegisterFormProps {
   schema: z.ZodSchema;
   error?: string;
   toggle: () => void;
+  onTestRegister: (type: "user" | "admin") => void;
   loading: boolean;
 }
 
@@ -25,6 +24,7 @@ const RegisterForm = ({
   schema,
   error,
   toggle,
+  onTestRegister,
   loading,
 }: RegisterFormProps) => {
   return (
@@ -45,21 +45,11 @@ const RegisterForm = ({
       <FormContainer
         onSubmit={onSubmit}
         resolver={zodResolver(registerValidationSchema)}
-        defaultValues={{ username: "", email: "", role: "", password: "" }}
+        defaultValues={{ username: "", email: "", password: "" }}
       >
         <div className="space-y-4">
           <FormInput name="username" label="Username" type="text" required />
-          <FormInput name="email" label="Email" type="email" required />
-          <FormSelect
-            label="Register As"
-            name="role"
-            placeholder="Select account type"
-            options={[
-              { label: "User", value: "USER" },
-              { label: "Agent", value: "AGENT" },
-            ]}
-            required
-          />
+          <FormInput name="email" label="Email Address" type="email" required />
           <FormInput
             name="password"
             label="Password"
@@ -68,10 +58,10 @@ const RegisterForm = ({
           />
         </div>
 
-        <div className="flex items-start text-xs text-gray-500 mt-2">
-          <input type="checkbox" className="mr-2 mt-0.5" />
+        <div className="flex items-center mt-3 mb-2 text-xs text-gray-500">
+          <input type="checkbox" className="mr-2" />
           <p>
-            By registering, you agree to our{" "}
+            I agree to the{" "}
             <Link href="#" className="text-slate-800 underline font-semibold">
               Terms & Privacy Policy
             </Link>
@@ -80,12 +70,31 @@ const RegisterForm = ({
 
         <Button
           type="submit"
-          className="w-full mt-4 bg-black text-white hover:bg-gray-900"
+          className="w-full bg-slate-800 text-white hover:bg-slate-700"
           disabled={loading}
         >
           {loading ? <Loader2 className="w-4 h-4 animate-spin" /> : "Register"}
         </Button>
       </FormContainer>
+
+      <div className="flex justify-between">
+        <Button
+          variant="outline"
+          className="bg-transparent border-slate-600 hover:bg-slate-800 hover:text-white hover:border-white rounded-full px-6 py-2 font-medium transition-all duration-200 group"
+          onClick={() => onTestRegister("user")}
+        >
+          User Demo
+          <ArrowRight className="ml-1 h-4 w-4 transition-transform duration-300 group-hover:translate-x-1" />
+        </Button>
+        <Button
+          variant="outline"
+          className="bg-transparent border-slate-600 hover:bg-slate-800 hover:text-white hover:border-white rounded-full px-6 py-2 font-medium transition-all duration-200 group"
+          onClick={() => onTestRegister("admin")}
+        >
+          Admin Demo
+          <ArrowRight className="ml-1 h-4 w-4 transition-transform duration-300 group-hover:translate-x-1" />
+        </Button>
+      </div>
 
       <p className="text-sm text-center mt-3">
         Already have an account?{" "}
