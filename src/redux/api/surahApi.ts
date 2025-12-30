375ab695c0d7decb978c4871b0d53fde2ad63458
Touhidur Zaman <touhidcodes@gmail.com>
2025-12-30 23:14:30 +0600

    mod: surah api

--- src/redux/api/surahApi.ts
+++ src/redux/api/surahApi.ts
@@ -1,9 +1,16 @@
+import { TApiResponse } from "@/types/api";
+import {
+  TCreateSurahPayload,
+  TSurah,
+  TUpdateSurahPayload,
+} from "@/types/surah";
 import { tagTypes } from "../tags";
 import { baseServerApi } from "./baseApi";
 
 export const surahApi = baseServerApi.injectEndpoints({
   endpoints: (build) => ({
-    getAllSurah: build.query({
+    // Get all surahs
+    getAllSurah: build.query<TApiResponse<TSurah[]>, void>({
       query: () => ({
         url: "/surah",
         method: "GET",
@@ -11,14 +18,56 @@ export const surahApi = baseServerApi.injectEndpoints({
       providesTags: [tagTypes.surah],
     }),
 
-    getSingleSurah: build.query({
+    // Get single surah by ID
+    getSingleSurah: build.query<TApiResponse<TSurah>, string>({
       query: (surahId) => ({
         url: `/surah/${surahId}`,
         method: "GET",
       }),
-      providesTags: [tagTypes.surah],
+      providesTags: (_result, _error, surahId) => [
+        { type: tagTypes.surah, id: surahId },
+      ],
+    }),
+    // Create surah
+    createSurah: build.mutation<TApiResponse<TSurah>, TCreateSurahPayload>({
+      query: (payload) => ({
+        url: "/surah",
+        method: "POST",
+        body: payload,
+      }),
+      invalidatesTags: [tagTypes.surah],
+    }),
+
+    // Update surah
+    updateSurah: build.mutation<
+      TApiResponse<TSurah>,
+      { surahId: string; payload: TUpdateSurahPayload }
+    >({
+      query: ({ surahId, payload }) => ({
+        url: `/surah/${surahId}`,
+        method: "PUT",
+        body: payload,
+      }),
+      invalidatesTags: (_result, _error, { surahId }) => [
+        { type: tagTypes.surah, id: surahId },
+      ],
+    }),
+
+    // Delete surah
+    deleteSurah: build.mutation<TApiResponse<null>, string>({
+      query: (surahId) => ({
+        url: `/surah/${surahId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.surah],
     }),
   }),
 });
 
-export const { useGetAllSurahQuery, useGetSingleSurahQuery } = surahApi;
+export const {
+  useGetAllSurahQuery,
+  useGetSingleSurahQuery,
+  useCreateSurahMutation,
+  useUpdateSurahMutation,
+  useDeleteSurahMutation,
+} = surahApi;
