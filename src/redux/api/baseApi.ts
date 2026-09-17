5b817e491df807d18dedc253e33f1d28c9cc2597
Touhidur Zaman <touhidcodes@gmail.com>
2026-09-17 21:32:19 +0600

    mod: auth api issues

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,29 +2,24 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { axiosBaseQuery } from "../../helpers/axios/axiosBaseQuery";
 import { tagTypesList } from "../tags";
 
-const baseUrl = `http://localhost:5000/api/v1`;
-// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
-console.log(baseUrl);
-
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
+const localUrl = process.env.NEXT_PUBLIC_LOCAL_URL!;
 const quranApiUrl = process.env.NEXT_PUBLIC_QURAN_API_URL!;
 
-// Define a service using a base URL and expected endpoints
 export const baseServerApi = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({
-    baseUrl,
+    localUrl,
   }),
   endpoints: () => ({}),
   tagTypes: tagTypesList,
 });
 
-// API Service 2 - Second Base URL
 export const baseQuranApi = createApi({
   reducerPath: "quranApi",
   baseQuery: axiosBaseQuery({
     baseUrl: quranApiUrl,
   }),
   endpoints: () => ({}),
-
   tagTypes: tagTypesList,
 });
