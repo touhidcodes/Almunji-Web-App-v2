eeec621817c9c385b702c4cc0f32fb6e84cb32fc
Touhidur Zaman <touhidcodes@gmail.com>
2026-09-18 19:20:07 +0600

    fix: auth token persistence

--- src/services/actions/userRegister.ts
+++ src/services/actions/userRegister.ts
@@ -1,7 +1,6 @@
-// "use server";
-
 import { FieldValues } from "react-hook-form";
 import setAccessToken from "./setAccessToken";
+import { toast } from "sonner";
 
 export const userRegister = async (data: FieldValues) => {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000/api/v1";
@@ -16,8 +15,14 @@ export const userRegister = async (data: FieldValues) => {
 
   const userInfo = await res.json();
 
+  if (userInfo.success === false) {
+    toast.error(userInfo.message || "Registration failed");
+    return userInfo;
+  }
+
   if (userInfo.data?.token) {
     setAccessToken(userInfo.data.token);
+    toast.success("Registration successful!");
   }
 
   return userInfo;
